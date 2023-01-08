0660f9ea19e6685f65a5758668213113f2a9a86d
Carmenbvg <carmenbvg@gmail.com>
2023-01-08 19:46:43 +0100

    Incluye comentarios con instrucciones

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -10,3 +10,9 @@ async def root():
 @app.get("/url")
 async def url_():
     return {"url_myweb":"https://trebool.wordpress.com/"}
+
+# Inicia el server: uvicorn main:app --reload
+# Detiene el server: CTRL+C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
